7dd7a0e47b391fa7386e384d15c066c696ec2258
fabiovandewaeter <vandewaeter.fabio@gmail.com>
2025-06-09 11:57:44 +0200

    change game URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { ResourceProvider } from './context/ResourceContext';
 import VillagePage from './pages/VillagePage';
 import CombatPage from './pages/CombatPage';
 import MiningPage from './pages/MiningPage';
 import StatsPage from './pages/StatsPage';
+import { VillagerProvider } from './context/VillagerContext';
 
 export default function App() {
   return (
     <ResourceProvider>
-      <BrowserRouter>
-        <div className="app-layout">
-          <nav className="sidebar">
-            <ul>
-              <li><Link to="/village" className="nav-link">🏠 Village</Link></li>
-              <li><Link to="/combat" className="nav-link">⚔️ Combat</Link></li>
-              <li><Link to="/mine" className="nav-link">⛏️ Miner</Link></li>
-              <li><Link to="/stats" className="nav-link">📊 Stats</Link></li>
-            </ul>
-          </nav>
+      <VillagerProvider>
+        <BrowserRouter basename="/TBRPG">
+          <div className="app-layout">
+            <nav className="sidebar">
+              <ul>
+                <li><Link to="/village" className="nav-link">🏠 Village</Link></li>
+                <li><Link to="/combat" className="nav-link">⚔️ Combat</Link></li>
+                <li><Link to="/mine" className="nav-link">⛏️ Miner</Link></li>
+                <li><Link to="/stats" className="nav-link">📊 Stats</Link></li>
+              </ul>
+            </nav>
 
-          <div className="main-content">
-            <Routes>
-              <Route path="/village" element={<VillagePage />} />
-              <Route path="/combat" element={<CombatPage />} />
-              <Route path="/mine" element={<MiningPage />} />
-              <Route path="/stats" element={<StatsPage />} />
-            </Routes>
+            <div className="main-content">
+              <Routes>
+                <Route path="/" element={<Navigate to="/village" replace />} />
+
+                <Route path="/village" element={<VillagePage />} />
+                <Route path="/combat" element={<CombatPage />} />
+                <Route path="/mine" element={<MiningPage />} />
+                <Route path="/stats" element={<StatsPage />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
-    </ResourceProvider>
+        </BrowserRouter>
+      </VillagerProvider>
+    </ResourceProvider >
   );
 }
